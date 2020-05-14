b7c5a06cb0d538d388931d511cbbb8f9c498e31f
P R, Prasad <prasad.p.r@intel.com>
2020-05-14 16:11:16 +0530

    modfied index.js file to display values

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("./models/User");
 require("./services/passport");
 const app = express();
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI).on();
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 3600 * 1000,
@@ -21,4 +21,11 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(
+	PORT,
+	console.log(
+		process.env.mongoURI,
+		process.env.GOOGLE_CLIENT_ID,
+		process.env.GOOGLE_CLIENT_SECRET
+	)
+);
