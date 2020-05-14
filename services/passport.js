7025d07f83e3c2cce0455582e6b9188759dc28d2
P R, Prasad <prasad.p.r@intel.com>
2020-05-14 16:26:10 +0530

    added parameter redirecturl

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,7 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: "/auth/google/callback",
+			callbackURL: keys.redirectURL + "/auth/google/callback",
 		},
 		(accessToken, refreshToken, profile, done) => {
 			User.findOne({ googleId: profile.id }).then((existingUser) => {
