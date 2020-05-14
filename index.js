5ba79dcaf7957446a44a2577f2c8bbf590c8be89
P R, Prasad <prasad.p.r@intel.com>
2020-05-14 15:50:37 +0530

    commitng code with authentication

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("hi there");
-});
+mongoose.connect(keys.mongoURI);
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 3600 * 1000,
+		keys: [keys.cookieKey],
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
